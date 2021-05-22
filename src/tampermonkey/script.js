f7f7b2fcfdbff871c9633e2c4989df193d8d8a95
Andrew <79267815+fuzzydragon@users.noreply.github.com>
2021-05-21 22:32:15 -0500

    Update script.js

--- src/tampermonkey/script.js
+++ src/tampermonkey/script.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://4chan.org/
+// @match        https://*.4chan.org/*
 // @icon         https://www.google.com/s2/favicons?domain=4chan.org
 // @grant        none
 // ==/UserScript==
